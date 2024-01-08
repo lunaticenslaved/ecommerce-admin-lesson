57a2168794801ef9dae22bf8ccb868cc6a6be7b3
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-08 23:22:39 +0300

    feat(billboards): add list of billboards

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,15 +1,18 @@
 'use client';
 
-import { Billboard } from '@prisma/client';
 import { Plus } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 
+import { ApiList } from '@/components/ui/api-list';
 import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
 
+import { BillboardColumn, columns } from './columns';
+
 interface BillboardClientProps {
-  data: Billboard[];
+  data: BillboardColumn[];
 }
 
 export function BillboardClient({ data }: BillboardClientProps) {
@@ -28,10 +31,10 @@ export function BillboardClient({ data }: BillboardClientProps) {
         </Button>
       </div>
       <Separator />
-      {/* <DataTable searchKey="label" columns={columns} data={data} /> */}
+      <DataTable searchKey="label" columns={columns} data={data} />
       <Heading title="API" description="API Calls for Billboards" />
       <Separator />
-      {/* <ApiList entityName="billboards" entityIdName="billboardId" /> */}
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 }
--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,40 @@
-import { BillboardClient } from './components/client';
+import { format } from 'date-fns';
 
-export default function BillboardsPage() {
+import { db } from '@/lib/prismadb';
+
+import { BillboardClient } from './components/client';
+import { BillboardColumn } from './components/columns';
+
+interface BillboardsPageProps {
+  params: {
+    storeId: string;
+  };
+}
+
+export default async function BillboardsPage({ params: { storeId } }: BillboardsPageProps) {
+  const billboards: BillboardColumn[] = await db.billboard
+    .findMany({
+      where: {
+        storeId,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+    .then(items =>
+      items.map(
+        (item): BillboardColumn => ({
+          id: item.id,
+          label: item.label,
+          createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+        }),
+      ),
+    );
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={[]} />
+        <BillboardClient data={billboards} />
       </div>
     </div>
   );
