84b23eced2e45dcba2f89f267f753a050741ea0f
lunaticenslaved <lunaticenslaved@yandex.ru>
2024-01-09 00:30:19 +0300

    feat(color): add colors

---
+++ app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,19 @@
+import { db } from '@/lib/prismadb';
+
+import { ColorForm } from './components/color-form';
+
+export default async function ColorPage({ params }: { params: { colorId: string } }) {
+  const color = await db.color.findUnique({
+    where: {
+      id: params.colorId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ColorForm initialData={color} />
+      </div>
+    </div>
+  );
+}
